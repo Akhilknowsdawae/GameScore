f1cd4f90f0bf6c41d17deb9403fa8899b541dfb3
marissecharette <124939657+marissecharette@users.noreply.github.com>
2024-03-02 20:49:59 -0500

    Created SignUp.html
    Created SignUp.html

--- GameScore/GameScoreApp/views.py
+++ GameScore/GameScoreApp/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.contrib import messages
+from django.contrib.auth import login, authenticate, logout
+from .forms import LoginForm, RegisterForm
 
 
 def mainPage(request):
 
     return render(request, 'index.html')
+
+def sign_up(request):
+    if request.method == 'GET':
+        form = RegisterForm()
+        return render(request, 'users/SignUp.html', { 'form': form})
